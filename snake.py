038004b347f993e9523ead962b4d190263be2d5e
Esther James <kemi.james11@gmail.com>
2026-05-08 23:12:40 +0100

    Foundational snake game, fully operational snake, functioning apple

--- snake.py
+++ snake.py
@@ -1,32 +1,39 @@
 # Python game
 import pygame
 from pygame.locals import *
+import random
 
 
-
+# --------------------------------------------------------------Player
 class Player:
-    #ddddd
-    x = []
-    # x axis
-    y = []
-    # y axis
-    step = 27
-    direction = 0
-    update_count_max = 2
-    update_count = 0
+    """Creates the player snake, and contains its controls"""
 
     def __init__(self,length):
+
         self.length = length
+        #ddddd
+        self.x = []
+        # x axis
+        self.y = []
+        # y axis
+        self.step = 27
+        self.direction = 0
+        self.update_count_max = 2
+        self.update_count = 0
+        self.start_x=300
+        self.start_y=300
         for i in range(0,length):
-            self.x.append(0)
-            self.y.append(0)
+            self.x.append(self.start_x)
+            self.y.append(self.start_y-(i*self.step))
 
     def update(self):
+        """The snake by default will move in a direction unless a key is pressed to change the direction"""
         self.update_count += 1
-
+        # It will be checked every 2 moves if the direction has changed, 
+        # so that the body of the snake will move according to the direction
         if self.update_count> self.update_count_max:
 
-            for i in range(self.length-1,0,-1):
+            for i in range(self.length-2,0,-1):
                 self.x[i] = self.x[i-1]
                 self.y[i] = self.y[i-1]
 
@@ -41,18 +48,48 @@ class Player:
                 self.y[0] = self.y[0] + self.step
         
             self.update_count = 0
+
     def move_right(self):
+        """Sets direction to the right of the screen"""
         self.direction = 0
     def move_left(self):
+        """Sets direction to the left of the screen"""
         self.direction = 1
     def move_up(self):
         self.direction = 2
     def move_down(self):
         self.direction = 3
-    def draw(self,surface,image):
-        for i in range(0,self.length):
-            surface.blit(image,(self.x[i],self.y[i]))
-    
+
+    # Draws the head as orange and body
+    def draw(self,surface,image,image2):
+        surface.blit(image,(self.x[0],self.y[0]))
+        for i in range(1,self.length-1):
+            surface.blit(image2,(self.x[i],self.y[i]))
+
+class Apple:
+    # Creates the objective of the game: get as many apples as possible without any collision
+    x = 0
+    y = 0
+    step = 27  
+    def __init__(self, x, y):
+        self.x = x * self.step 
+        self.y = y * self.step
+    def draw(self, surface, image):
+        surface.blit(image,(self.x, self.y)) 
+
+class Game:
+    def is_collision(self, x1, x2, y1, y2, bsize):
+            if x1 >= x2 and x1 <= x2 + bsize:
+                if y1 >= y2 and y1 <= y2 + bsize:
+                    return True
+            return False
+
+    def out_of_bounds_check(self, w_width, w_height, x, y):
+        if x > w_width or x < 0:
+            return True
+        if y > w_height or y < 0:
+            return True
+        return False
 
 class App: 
     window_width = 800
@@ -62,8 +99,12 @@ class App:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self._image_surf = None
+        self._body_surf = None
+        self._apple_surf = None
+        self._head_surf = None
+        self.game = Game()
         self.player = Player(10)
+        self.apple = Apple(5,5)
         self.clock = pygame.time.Clock()
     
     def on_init(self):
@@ -71,8 +112,13 @@ class App:
         self._display_surf = pygame.display.set_mode((self.window_width,self.window_height),pygame.HWSURFACE)
         pygame.display.set_caption('Pygame pythonspot.com example')
         self._running = True
-        self._image_surf = pygame.Surface((25, 25))
-        self._image_surf.fill((255, 0, 0))
+        self._head_surf = pygame.Surface((25, 25))
+        self._head_surf.fill((255, 153, 28))
+        self._body_surf = pygame.Surface((25, 25))
+        self._body_surf.fill((255, 0, 0))
+        self._apple_surf = pygame.Surface((25, 25))
+        self._apple_surf.fill((0, 255, 0))
+
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -81,9 +127,39 @@ class App:
     def on_loop(self):
         self.player.update()
 
+        # Detects if the snake has reached an apple
+        for i in range(0,self.player.length):
+            if self.game.is_collision(self.player.x[i], self.apple.x, self.player.y[i], self.apple.y, 25):
+                self.apple.x = random.randint(2,9) * 25
+                self.apple.y = random.randint(2,9) * 25
+                self.player.length = self.player.length + 1
+                # Another block will be added based on the direction of movement
+                self.player.x.append(self.player.x[self.player.length-2])
+                self.player.y.append(self.player.y[self.player.length-2])
+                
+ 
+        # Detects is the snake has collided with itself (game ends)
+        for i in range(4,self.player.length):
+            if self.game.is_collision(self.player.x[0],self.player.x[i],self.player.y[0], self.player.y[i],25):
+                print("You lose! Collision: ")
+                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+                exit(0)
+        # Detects if the snake has touched the game window boarders (game ends)
+        if self.game.out_of_bounds_check(self.window_width, self.window_height, self.player.x[0], self.player.y[0]):
+            print("You have collided with the wall, game over")
+            exit(0)
+ 
+        pass
+
+
+
+
     def on_render(self):
+        """Runs when the game opens"""
         self._display_surf.fill((0,0,0))
-        self.player.draw(self._display_surf,self._image_surf)
+        self.player.draw(self._display_surf, self._head_surf,self._body_surf)
+        self.apple.draw(self._display_surf,self._apple_surf)
         pygame.display.flip()
 
     def on_cleanup(self):
@@ -97,23 +173,23 @@ class App:
             pygame.event.pump()
             keys = pygame.key.get_pressed()
 
-            if (keys[K_RIGHT]):
+            if (keys[K_d]):
                 self.player.move_right()
 
-            if (keys[K_LEFT]):
+            if (keys[K_a]):
                 self.player.move_left()
 
-            if (keys[K_UP]):
+            if (keys[K_w]):
                 self.player.move_up()
 
-            if (keys[K_DOWN]):
+            if (keys[K_s]):
                 self.player.move_down()
 
             if (keys[K_ESCAPE]):
                 self._running =  False
             self.on_loop()
             self.on_render()
-            self.clock.tick(60)
+            self.clock.tick(45)
         self.on_cleanup()
 
 
